b29b2b8ea757d21193addf22bed6fa163ff756fc
xuan <exxk.lx@gmail.com>
2018-04-17 00:25:09 +0800

    创建zuul工程

---
+++ eurekazuul/src/main/java/exxk/could/eurekazuul/EurekazuulApplication.java
@@ -0,0 +1,12 @@
+package exxk.could.eurekazuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekazuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekazuulApplication.class, args);
+    }
+}
