0d11761dd4f391cd919aab749b57b90e6d17fa1b
xuan <exxk.lx@gmail.com>
2018-04-22 18:34:11 +0800

    更新

--- configclient/src/main/java/exxk/could/configclient/ConfigclientApplication.java
+++ configclient/src/main/java/exxk/could/configclient/ConfigclientApplication.java
@@ -2,9 +2,8 @@ package exxk.could.configclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-@RefreshScope
+
 @EnableEurekaClient
 @SpringBootApplication
 public class ConfigclientApplication {
--- configclient/src/main/java/exxk/could/configclient/HiController.java
+++ configclient/src/main/java/exxk/could/configclient/HiController.java
@@ -4,7 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+@RefreshScope
 @RestController
 public class HiController {
     @Value("${test}") //调用配置中心的配置
