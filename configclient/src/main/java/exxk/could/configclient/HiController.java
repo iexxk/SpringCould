0a6a3f7e1290544c128f42ba50d1ccbfe7564c41
xuanleung <exxk.lx@gmail.com>
2020-08-07 15:59:27 +0800

    添加公共配置文件

--- configclient/src/main/java/exxk/could/configclient/HiController.java
+++ configclient/src/main/java/exxk/could/configclient/HiController.java
@@ -4,14 +4,21 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
 @RefreshScope
 @RestController
 public class HiController {
     @Value("${test}") //调用配置中心的配置
-    String test;
+            String test;
+
+    @Value("${common}") //调用配置中心的配置
+            String common;
 
     @RequestMapping(value = "/hi")
-    public String hi(){
-        return test;
+    public List<String> hi() {
+        return Arrays.asList(test, common);
     }
 }
